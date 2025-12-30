3aaa39d6c54351d9dea92b94a74d6196647a552c
Anderson <ilesanderson01@gmail.com>
2025-12-30 11:31:12 -0500

    Consolidación sobre asincronía

---
+++ exercises/async-practice.js
@@ -0,0 +1,186 @@
+// #region Asincronía con Callbacks 
+
+// function anyCallBack(parametro1){
+//     setTimeout(()=>{
+//         parametro1()
+//     },2000)
+// }
+// function anyCallBackOne(){
+//     console.log("Primero en ejecutarse")
+//     anyCallBack(()=>{
+//         console.log("Segundo en ejecutarse")
+//         anyCallBack(()=>{
+//             console.log("Tercero en ejecutarse")
+//             anyCallBack(()=>{
+//                 console.log("Cuarto en ejecutarse")
+//             })
+//             console.log("Ejecutado 3")
+//         })
+//         console.log("Ejecutado 2")
+//     })
+//     console.log("Ejecutado 1")
+// }
+
+// anyCallBack(anyCallBackOne)
+
+// #region Asincronía con Promises
+
+// const exNumber=12
+// //Primera manera para escribir Promises
+// const exPromise= new Promise((resolve,reject)=>{
+//     // Comprobación del valor de exNumber
+//     if(exNumber===12){
+//         console.log("Primera ejecución")
+//         setTimeout(()=>{
+//             // Llamada a la función exPromise2 y resolución de la promesa
+//             resolve(exPromise2())
+//         },0)
+//         console.log("se finalizó la primera promesa")
+//     }
+//     else{
+//        console.log("Segunda ejecución")
+//        setTimeout(()=>{
+//         // Rechazo de la promesa con un mensaje de error
+//            reject("No manches puerco")
+//        },5000)
+//     }
+// })
+// //Segunda manera para escribir Promises
+// const exPromise2= ()=>{
+//     return new Promise((resolve,reject)=>{
+//         // Se ejecuta esta parte del código al resolverse la primera promesa
+//         console.log("Tercera ejecucción")
+//         // Se resuelve la segunda promesa con un mensaje
+//         setTimeout(()=>{
+//             resolve("Bien hecho puerco, se acabo")
+//         },5000)
+//     })
+// }
+// //Manejo de la primera promesa
+// exPromise
+// .then((result)=>{
+//     console.log(result)
+//     throw new Error("No jodas")
+// })
+// .then((result2)=>
+//     console.log(result2)
+// )
+// .catch((parametre)=>{
+//     console.log("Manejo del error")
+//     console.log(parametre)
+// }) 
+
+
+// #region Asincronía con Async/Await
+
+// async function explotarGlobol(){
+//     console.log("Empezando a explotar globos")
+//     const globo1=await globo1Promise;
+//     const globo2=await globo2Promise;
+//     console.log(globo1);
+//     console.log(globo2);
+//     setTimeout(()=>{
+//         console.log("A ver dijo el ciego")
+//     },2000)
+// }
+// function explotarGloboPromises(ms, mensaje,aviso){
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(mensaje);
+//         }, ms);
+//         console.log(aviso);
+//     });
+// }
+// const globo1Promise = explotarGloboPromises(3000, "Exploto globo 1", "promise 1 creada");
+// const globo2Promise = explotarGloboPromises(5000, "Exploto globo 2", "promise 2 creada");
+// explotarGlobol()
+
+// Otro Ejemplo de Async/Await
+
+//                                      Almacen de Pizzas
+// let valueLogic= false;
+// const esperar= async (ms,mensaje)=>{
+//     await new Promise((resolve)=> setTimeout(resolve,ms));
+//     if(!valueLogic){
+//         throw new Error("Error en el proceso de la pizza");
+//     }
+//     return mensaje;
+// }
+// const ordenarPizza= async ()=>{
+//     console.log("Iniciando orden de pizza");
+//     try{
+//     const verificarInventario= await esperar(4000,"Inventario verificado");
+//     console.log(verificarInventario);
+//     const procesarPago= await esperar(2000,"Pago procesado");
+//     console.log(procesarPago);
+//     const prepararPizza= await esperar(6000,"Pizza preparada");
+//     console.log(prepararPizza);
+//     console.log("Disfruta tu pizza, brou");
+//     return "Ta bien ";
+//     }
+//     catch(error){
+//         console.log(error);
+//         return error;
+//     }
+// };
+// ordenarPizza()
+
+
+// #region Asincronía con Promise.all
+//                    Preparación de Ingredientes para una receta
+// let thereIsError= true;
+// const proceso= async (ms, mensaje) => {
+//     await new Promise((resolve) => setTimeout(resolve, ms));
+//     if (!thereIsError) {
+//         throw new Error("Error en la preparación de ingredientes");
+//     }
+//     console.log(mensaje);
+// };
+// const prepararIngredientes= async ()=>{
+//     const prepararTomate= proceso(4000,"Tomate preparado");
+//     const prepararQueso= proceso(2000,"Queso preparado");
+//     const prepararJamon= proceso(2000,"Jamón preparado");
+//     try{
+//         const resultado=await Promise.all([prepararTomate, prepararQueso, prepararJamon]);
+//         console.log("Ingredientes listos, a cocinar");
+//         return resultado;
+//     }
+//     catch(error){
+//         console.log(error);
+//         console.log("No se pudo preparar los ingredientes");
+//     }
+// };
+// prepararIngredientes()
+
+// #region Ejecicios con Promise.allSetted
+//                    Evaluación de Estudiantes
+const students=[
+    {fullName:"Joel Páez",score:9,timeResolved:2000},
+    {fullName:"Ximena Perez",score:7,timeResolved:3000},
+    {fullName:"PaulCasanova",score:10,timeResolved:1000},
+    {fullName:"Maria Castillo",score:6,timeResolved:4000},
+];
+
+const getScore= async (score,timeResolved,nameStudent)=>{
+    await new Promise((resolve)=> setTimeout(resolve,timeResolved));
+    if(score<8){
+        return new Error(`El Estudiante ${nameStudent} obtuvo:${score} y es un pena, y su padre es el negro Jose xdxdxd`);
+    }
+    return `El Estudiante ${nameStudent} obtuvo:${score}, por lo tanto ¡Bien hecho puerco!`;
+}
+const getReview= async ()=>{
+    const studentOne=await getScore(students[0].score,students[0].timeResolved,students[0].fullName);
+    const studentTwo=await getScore(students[1].score,students[1].timeResolved,students[1].fullName);
+    const studentThree=await getScore(students[2].score,students[2].timeResolved,students[2].fullName);
+    const studentFour=await getScore(students[3].score,students[3].timeResolved,students[3].fullName);
+    try {
+        const result=await Promise.all([studentOne,studentTwo,studentThree,studentFour]);
+        console.log(result);
+        console.log("Todos los estudiantes han sido evaluados");
+    }
+    catch(error){
+        console.log(error);
+    }
+
+}
+getReview()
